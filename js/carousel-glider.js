5d6208e3df6f2b40e8283e3ef1bd25c157ed8e38
angelgt <angel.gamonal@agenciaservitec.com>
2022-11-23 22:30:54 -0500

    subiendo proyecto final S2

---
+++ js/carousel-glider.js
@@ -0,0 +1,78 @@
+window.addEventListener('load', function () {
+	new Glider(document.querySelector('.carousel-list-glider'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__previous',
+			next: '.carousel__next',
+		},
+		responsive: [
+			{
+				// screens greater than >= 775px
+				breakpoint: 580,
+				settings: {
+					// Set to `auto` and provide item width to adjust to viewport
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				// screens greater than >= 1024px
+				breakpoint: 1115,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				},
+			},
+			{
+				// screens greater than >= 1024px
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4,
+				},
+			},
+		],
+	});
+});
+
+//Similar products
+window.addEventListener('load', function () {
+	new Glider(document.querySelector('.carousel-list-similar-products'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__previous',
+			next: '.carousel__next',
+		},
+		responsive: [
+			{
+				// screens greater than >= 775px
+				breakpoint: 580,
+				settings: {
+					// Set to `auto` and provide item width to adjust to viewport
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				// screens greater than >= 1024px
+				breakpoint: 1115,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				},
+			},
+			{
+				// screens greater than >= 1024px
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4,
+				},
+			},
+		],
+	});
+});
